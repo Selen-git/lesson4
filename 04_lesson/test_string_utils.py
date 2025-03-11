73de4f535a0859173ad737e537d86cb59d91fc01
Selen-git <Selen_6966@inbox.ru>
2025-03-11 22:13:53 +0500

    Update test_string_utils.py

--- 04_lesson/test_string_utils.py
+++ 04_lesson/test_string_utils.py
@@ -1,1 +1,32 @@
+class StringUtils:
+   
 
+    def capitalize(self, string: str) -> str:
+       capitilize("skypro") -> "Skypro"
+        return string.capitalize()
+
+    def trim(self, string: str) -> str:
+        trim("   skypro") -> "skypro"
+        whitespace = " "
+        while string.startswith(whitespace):
+            string = string.removeprefix(whitespace)
+        return string
+
+    def contains(self, string: str, symbol: str) -> bool:
+        string - строка для обработки
+        symbol - искомый символ
+        contains("SkyPro", "S") -> True
+        contains("SkyPro", "U") -> False
+        res = False
+        try:
+            res = string.index(symbol) > -1
+        except ValueError:
+            pass
+        return res
+
+    def delete_symbol(self, string: str, symbol: str) -> str:
+        delete_symbol("SkyPro", "k") -> "SyPro"
+        delete_symbol("SkyPro", "Pro") -> "Sky"
+        if self.contains(string, symbol):
+            string = string.replace(symbol, "")
+        return string
